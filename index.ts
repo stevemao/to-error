71926cee21d24621ace23772eef4d216ec1e961e
Steve Mao <maochenyan@gmail.com>
2020-09-10 22:10:08 +1000

    safe stringify

--- index.ts
+++ index.ts
@@ -1,2 +1,4 @@
+import * as stringify from 'json-stringify-safe'
+
 export default (e: unknown): Error =>
-    e instanceof Error ? e : new Error(JSON.stringify(e));
+    e instanceof Error ? e : new Error(stringify(e));
